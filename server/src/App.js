6a9f95d1f84bdf774ba7564b6a1b2d4e4f4044a5
abbaskumar <kumarabbas1019@gmail.com>
2025-08-31 03:38:08 +0530

    hjgh
    pklkj

---
+++ server/src/App.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const workerRouter = require("./routers/signup_workers");
+const routes = require("./routers/signupCustomer");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const book_service = require("./routers/book-services");
+require("./db/mongoose");
+require("dotenv").config();
+const bookingService=require("./routers/booking-service")
+// Creating Express App
+app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(routes);
+app.use(workerRouter);
+app.use(book_service);
+app.use(bookingService);
+const port = process.env.port || 5000;
+
+app.get("/", (req, res) => {
+	res.send("Hello world");
+});
+
+app.listen(port, () => {
+	console.log("Server is up on the port " + port);
+});
